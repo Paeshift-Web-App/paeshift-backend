8ce71e13e06081e02c20aad279e6123e283cd98c
Folorunso Timson <timsonfoli@gmail.com>
2025-02-02 18:35:55 +0100

    Job Listing Page urls.py modified with actions

--- JobListingPage/urls.py
+++ JobListingPage/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import UserProfileViewSet, JobViewSet, JobApplicationViewSet, SavedJobViewSet
+
+
+
+
+# urlpatterns = [
+#     path("api/userProfile/", UserProfileViewSet.as_view()),
+#     path("api/job/", JobViewSet.as_view()),
+#     path("api/jobApplication/", JobApplicationViewSet.as_view()),
+#     path("api/savedJob/", SavedJobViewSet.as_view()),
+# ]
+
+
+from django.urls import path
 from .views import UserProfileViewSet, JobViewSet, JobApplicationViewSet, SavedJobViewSet
 
-
-
-
 urlpatterns = [
-    path("api/userProfile/", UserProfileViewSet.as_view()),
-    path("api/job/", JobViewSet.as_view()),
-    path("api/jobApplication/", JobApplicationViewSet.as_view()),
-    path("api/savedJob/", SavedJobViewSet.as_view()),
+    path("api/userProfile/", UserProfileViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("api/job/", JobViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("api/jobApplication/", JobApplicationViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("api/savedJob/", SavedJobViewSet.as_view({'get': 'list', 'post': 'create'})),
 ]
